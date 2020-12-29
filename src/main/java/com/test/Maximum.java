0528c517fe37d55fc1ad66e84f3071905bd7d012
souvik1232 <souvik199831@gmail.com>
2020-12-29 10:58:16 +0530

    Refractored:Generic method used for testcase

--- src/main/java/com/test/Maximum.java
+++ src/main/java/com/test/Maximum.java
@@ -1,45 +1,12 @@
 package com.test;
 
 public class Maximum {
-    public static int maxInteger(int array[] ) {
-        int temp = 0;
-        for (int i =0 ; i< array.length;i++){
-            for (int j = 1; j< array.length;j++){
-                if(array[j-1] < array[j]){
-                    temp = array[j-1];
-                    array[j-1] = array[j];
-                    array[j] = temp;
-                }
-            }
+    public static <E extends Comparable<E>> E maxValue(E[] array){
+        E temp = array[0];
+        for (int i =1 ; i< array.length;i++){
+            if (temp.compareTo(array[i])<0)
+                temp=array[i];
         }
-        return array[0];
-    }
-
-    public static float maxFloat(float[] array) {
-        float temp = 0;
-        for (int i =0 ; i< array.length;i++){
-            for (int j = 1; j< array.length;j++){
-                if(array[j-1] < array[j]){
-                    temp = array[j-1];
-                    array[j-1] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-        return array[0];
-    }
-
-    public static String maxString(String [] array) {
-        String temp = null;
-        for (int i =0 ; i< array.length;i++){
-            for (int j = 1; j< array.length;j++){
-                if(array[j-1].length() < array[j].length()){
-                    temp = array[j-1];
-                    array[j-1] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-        return array[0];
+        return temp;
     }
 }
--- src/test/java/com/test/MaximumTest.java
+++ src/test/java/com/test/MaximumTest.java
@@ -9,53 +9,54 @@ public class MaximumTest {
     //UC1
     @Test//TC1
     public void testIntegerMaximum1() {
-        int arr[] = {1,2,3};
+        Integer arr[] = {1,2,3};
         int result = 0;
         Maximum maximum = new Maximum();
-        result = maximum.maxInteger(arr);
-        Assert.assertEquals(3,result);
+        result = maximum.maxValue(arr);
+        Assert.assertEquals(result,3);
     }
 
     @Test//TC2
     public void testIntegerMaximum2() {
-        int arr[] = {1,3,2};
+        Integer arr[] = {1,3,2};
         int result = 0;
         Maximum maximum = new Maximum();
-        result = maximum.maxInteger(arr);
-        Assert.assertEquals(3,result);
+        result = maximum.maxValue(arr);
+        assertEquals(3,result);
     }
     @Test//TC3
     public void testIntegerMaximum3() {
-        int arr[] = {3,1,2};
+        Integer arr[] = {3,1,2};
         int result = 0;
         Maximum maximum = new Maximum();
-        result = maximum.maxInteger(arr);
+        result = maximum.maxValue(
+                arr);
         Assert.assertEquals(3,result);
     }
 
     //UC2
     @Test//TC1
     public void testFloatMaximum1() {
-        float arr[] = {1.0F, 2.0F, 3.0F};
+        Float arr[] = {1.0F, 2.0F, 3.0F};
         float result = 0.0F;
         Maximum maximum = new Maximum();
-        result = maximum.maxFloat(arr);
+        result = maximum.maxValue(arr);
         Assert.assertEquals(3.0F,result,0);
     }
     @Test//TC2
     public void testFloatMaximum2() {
-        float arr[] = {1.0F, 3.0F, 2.0F};
+        Float arr[] = {1.0F, 3.0F, 2.0F};
         float result = 0.0F;
         Maximum maximum = new Maximum();
-        result = maximum.maxFloat(arr);
+        result = maximum.maxValue(arr);
         Assert.assertEquals(3.0F,result,0);
     }
     @Test//TC3
     public void testFloatMaximum3() {
-        float arr[] = {3.0F, 2.0F, 1.0F};
+        Float arr[] = {3.0F, 2.0F, 1.0F};
         float result = 0.0F;
         Maximum maximum = new Maximum();
-        result = maximum.maxFloat(arr);
+        result = maximum.maxValue(arr);
         Assert.assertEquals(3.0F,result,0);
     }
 
@@ -65,23 +66,23 @@ public class MaximumTest {
         String arr[] = {"apple","banana","peach"};
         String result = null;
         Maximum maximum = new Maximum();
-        result = maximum.maxString(arr);
-        Assert.assertEquals("banana",result);
+        result = maximum.maxValue(arr);
+        Assert.assertEquals("peach",result);
     }
     @Test//TC2
     public void testStringMaximum2() {
         String arr[] = {"banana","apple","peach"};
         String result = null;
         Maximum maximum = new Maximum();
-        result = maximum.maxString(arr);
-        Assert.assertEquals("banana",result);
+        result = maximum.maxValue(arr);
+        Assert.assertEquals("peach",result);
     }
     @Test//TC3
     public void testStringMaximum3() {
         String arr[] = {"apple","peach","banana"};
         String result = null;
         Maximum maximum = new Maximum();
-        result = maximum.maxString(arr);
-        Assert.assertEquals("banana",result);
+        result = maximum.maxValue(arr);
+        Assert.assertEquals("peach",result);
     }
 }
